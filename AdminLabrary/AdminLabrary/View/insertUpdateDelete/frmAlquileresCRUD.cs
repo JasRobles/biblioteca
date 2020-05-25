4e5dc4931c1b9a1bb32e65df0638f935a346e1dd
JasRobles <63456629+JasRobles@users.noreply.github.com>
2020-05-25 16:00:00 -0600

    libros

---
+++ AdminLabrary/AdminLabrary/View/insertUpdateDelete/frmAlquileresCRUD.cs
@@ -0,0 +1,33 @@
+﻿using AdminLabrary.View.buscar;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdminLabrary.View.insertUpdateDelete
+{
+    public partial class frmAlquileresCRUD : Form
+    {
+        public int IdLibro;
+        public frmAlquileresCRUD()
+        {
+            InitializeComponent();
+        }
+        frmBuscarLibros li = new frmBuscarLibros();
+        private void btnSeleccionarLibro_Click(object sender, EventArgs e)
+        {
+            li.ShowDialog();
+        }
+        frmBuscarLector lec = new frmBuscarLector();
+        private void btnSeleccionarLector_Click(object sender, EventArgs e)
+        {
+            lec.indicador = 2;
+            lec.ShowDialog();
+        }
+    }
+}
